b5ea6664296ee9ef17761d6cfd778785a3c294bb
eduardo.arruda.souza <eduardo.arruda.souza@accenture.com>
2018-12-18 09:43:20 -0300

    Initial commit

---
+++ app/src/main/java/com/eas/bottomnavigation/bottomnavigation/BlankFragment.kt
@@ -0,0 +1,31 @@
+package com.eas.bottomnavigation.bottomnavigation
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+class BlankFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_blank, container, false)
+        val tvMensagem = view.findViewById<TextView>(R.id.tvMensagem)
+        tvMensagem.text = arguments?.getString("texto")
+        return view
+    }
+
+    companion object {
+        fun newInstance(texto: String) : BlankFragment {
+            val fragment = BlankFragment()
+            val bundle = Bundle()
+            bundle.putString("texto", texto)
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+
+}
